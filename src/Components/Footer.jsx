7cb8588a61d0e27b1fcc39aaa751b38d3333b0ea
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2023-07-29 22:02:48 +0800

    Added api on api folder and added per item page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="mt-5 py-4">
+      <div className="follow-us">
+        <div className="linked-in"></div>
+        <div className="instagram"></div>
+        <div className="facebook"></div>
+        <div className="twitter"></div>
+        <div className="whats-up"></div>
+        <div className="tiktok"></div>
+        <div className="youtube"></div>
+      </div>
+      <div className="footer-options">
+        <div className="privacy">Privacy</div>
+        <div className="terms">Terms of Use</div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/pages/products/productPage.jsx
+++ src/pages/products/productPage.jsx
@@ -19,37 +19,41 @@ function productPage() {
   const productsCards = useMemo(() => {
     return products.map((item) => {
       return (
-        <div className="card" key={item.id}>
-          <div className="productImage">
-            <Image src={item.images[0]} alt="image" width="320" height="165" />
-          </div>
-          <div className="productDetails py-1 px-3">
-            <h2 className="productName">{item.title}</h2>
-            <div className="productDesc">{item.description}</div>
-            <div className="productPrice">
-              <h2>${item.price}</h2>
+        <a href={`/products/${item.id}`} className="text-decoration-none">
+          <div className="card" key={item.id}>
+            <div className="productImage ">
+              <Image
+                src={item.images[0]}
+                alt="image"
+                width={320}
+                height={160}
+              />
+            </div>
+            <div className="productDetails d-flex flex-column flex-wrap py-1 px-3">
+              <h2 className="productName">{item.title}</h2>
+              <div className="productDesc">{item.description}</div>
+              <h2 className="productPrice h-auto">${item.price}</h2>
+            </div>
+            <div className="productRatings py-1 px-3">
+              <div className="stars">
+                <div className="star"></div>
+                <div className="starsAverage">{item.rating}</div>
+                <div className="salesCount">{item.stock} left in stock</div>
+              </div>
+              <div className="buy-item"></div>
             </div>
           </div>
-          <div className="productRatings">
-            <div className="stars">
-              <div className="star"></div>
-              <div className="starsAverage">{item.rating}</div>
-              <div className="salesCount">{item.stock} left in stock</div>
-            </div>
-            <div className="buy-item"></div>
-          </div>
-        </div>
+        </a>
       );
     });
   }, [products]);
   return (
     <div>
       <Header user={user} />
-      <div className="productsSection">
+      <div className="productsSection p-2">
         <ProductOptions user={user} />
         <div className="productsHeader">All Products</div>
-        <div className="products d-flex justify-content-evenly align-items-center flex-wrap">
-          <a href="/landing" className="text-decoration-none"></a>
+        <div className="products d-flex gap-4 justify-content-evenly align-items-start flex-wrap">
           {productsCards}
         </div>
       </div>
