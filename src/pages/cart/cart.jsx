8fcbbddcabb59d0c43af42d0df40fe262ef72ac2
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2023-07-31 16:48:32 +0800

    added order-placed/suceed styles and finished product details on laptop view

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -20,63 +20,77 @@ function cart() {
     <div>
       <Header user={user} />
       <ProductOptions user={user} />
-      {/* <a href="/api/auth/logout">
+      <div className="product">
+        {/* <a href="/api/auth/logout">
         <button>signout</button>
       </a> */}
-      <div className="cart-client p-4">
-        <div className="cart-client-header">
-          <div className="h1">Shopping Bag</div>
-          <span>4 items in the shopping bag</span>
-        </div>
-        <div className="shopping-cart">
-          <div className="card ">
-            <div className="row card-body align-items-center">
-              <div className="col">
-                <Image src="" alt="item" height="53" width="53" />
-              </div>
-              <div className="col-6">
-                <div className="row">
-                  <h5>Samsung Universe 9</h5>
-                  <div className="d-flex quantityButtons">
-                    <button>+</button>
-                    <span id="itemCount">1</span>
-                    <button>-</button>
+        <div className="cart-client p-4">
+          <div className="cart-client-header">
+            <div className="h1">Shopping Bag</div>
+            <span>4 items in the shopping bag</span>
+          </div>
+          <div className="shopping-cart">
+            <div className="card w-auto">
+              <div className="row card-body align-items-center">
+                <div className="col">
+                  <Image src="" alt="item" height="53" width="53" />
+                </div>
+                <div className="col-6">
+                  <div className="row">
+                    <h5>Samsung Universe 9</h5>
+                    <div className="d-flex quantityButtons">
+                      <button>+</button>
+                      <span id="itemCount">1</span>
+                      <button>-</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col">$1,249</div>
-              <div className="col d-flex justify-content-center align-items-center">
-                <FontAwesomeIcon icon={faCircleXmark} size="2x" />
-              </div>
-              {/* <button type="button" className="col-4 remove-item">
+                <div className="col">$1,249</div>
+                <div className="col d-flex justify-content-center align-items-center">
+                  <FontAwesomeIcon icon={faCircleXmark} size="2x" />
+                </div>
+                {/* <button type="button" className="col-4 remove-item">
                 x
               </button> */}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="details">
-          <div className="totalCost d-flex align-items-center gap-4">
-            <span>Total:</span>
-            <span>$12498</span>
-          </div>
-          <div className="sign-in-status">
-            <span>
-              To place an order, <a href="/sign-in/signin">sign in</a>
-            </span>
-          </div>
-          <div className="place-order-buttons">
-            <a
-              href="/order-placed/success"
-              className="place-order-btn text-decoration-none"
-            >
-              Place order (Add Onclick function for this)
-            </a>
-            <a
-              href="/products/productPage"
-              className="continue-shopping text-decoration-none"
-            >
-              Continue shopping
-            </a>
+          <div className="details">
+            <div className="totalCost d-flex align-items-center gap-4">
+              <span>Total:</span>
+              <span>$12498</span>
+            </div>
+
+            {user.user === undefined ? (
+              <div className="sign-in-status">
+                <span>
+                  To place an order, <a href="/sign-in/signin">sign in</a>
+                </span>
+              </div>
+            ) : (
+              <div className="sign-in-status"></div>
+            )}
+
+            <div className="place-order-buttons">
+              {user.user === undefined ? (
+                <button className="place-order-btn" disabled>
+                  Place order (Add Onclick function for this)
+                </button>
+              ) : (
+                <a
+                  href="/order-placed/success"
+                  className="place-order-btn text-decoration-none"
+                >
+                  Place order (Add Onclick function for this)
+                </a>
+              )}
+              <a
+                href="/products/productPage"
+                className="continue-shopping text-decoration-none"
+              >
+                Continue shopping
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { addItemToCart } from "../../feature/storeSlice";
 
 export const getServerSideProps = async (context) => {
   const postId = context.params?.id;
@@ -18,6 +20,7 @@ export const getServerSideProps = async (context) => {
 };
 
 export default function Post({ data }) {
+  const dispatch = useDispatch();
   const [productNumber, setProductNumber] = useState(1);
   const user = useUser();
   const addItemQuantityHandler = () => {
@@ -29,70 +32,80 @@ export default function Post({ data }) {
       : setProductNumber(productNumber - 1);
   };
   return (
-    <div className="product-item">
+    <div>
       <Header user={user} />
       <ProductOptions user={user} />
-      <div className="product-image pb-5 d-flex align-items-center justify-content-center">
-        <Image
-          className="rounded-3 grid place-items-center"
-          src={data.images[0]}
-          alt={`image${data.id}`}
-          height={191}
-          width={300}
-        />
-      </div>
-      <div className="product-price d-flex align-items-center gap-4">
-        <h1 className="m-2">${data.price}</h1>
-        <div className="discount-percentage bg-primary rounded-5 py-1 px-2">
-          {data.discountPercentage}%
+      <div className="product-item">
+        <div className="product-image pb-5 d-flex align-items-center justify-content-center">
+          <Image
+            className="rounded-3 grid place-items-center"
+            src={data.images[0]}
+            alt={`image${data.id}`}
+            height={191}
+            width={300}
+          />
         </div>
-      </div>
-
-      <div className="btn btn-lg g-0 w-100 product-add-to-cart bg-primary rounded-2 text-center ">
-        {/* add height for this button  */}
-        <div className="add-to-cart-options">
-          <div className="row">
-            <button className="col" onClick={addItemQuantityHandler}>
-              +
-            </button>
-            <span className="col">{productNumber}</span>
-            <button className="col" onClick={minusItemQuantityHandler}>
-              -
-            </button>
-            <FontAwesomeIcon
-              className="col shopping-cart-icon"
-              icon={faShoppingCart}
-            />
+        <div className="product-price d-flex align-items-center gap-4">
+          <h1 className="m-2">${data.price}</h1>
+          <div className="discount-percentage rounded-5 py-1 px-2 color-13">
+            {data.discountPercentage}%
           </div>
         </div>
-        <span className="add-to-cart-text">Add to Cart</span>
+        <div className="product-buy-now">
+          <div className="btn btn-lg g-0 w-100 product-add-to-cart bg-primary rounded-2 text-center">
+            {/* add height for this button  */}
+            <div className="add-to-cart-options ">
+              <div className="row">
+                <button className="col btn" onClick={addItemQuantityHandler}>
+                  +
+                </button>
+                <span className="col d-flex align-items-center justify-content-center">
+                  {productNumber}
+                </span>
+                <button className="col btn" onClick={minusItemQuantityHandler}>
+                  -
+                </button>
+                <FontAwesomeIcon
+                  className="col shopping-cart-icon"
+                  icon={faShoppingCart}
+                />
+              </div>
+            </div>
+            <span className="add-to-cart-text">Add to Cart</span>
+          </div>
+        </div>
+        <div className="other-images d-flex justify-content-center gap-2 my-2 mx-1">
+          <Image
+            className="rounded-2"
+            src={data.images[1]}
+            height={93}
+            width={93}
+          />
+          <Image
+            className="rounded-2"
+            src={data.images[2]}
+            height={93}
+            width={93}
+          />
+          <Image
+            className="rounded-2"
+            src={data.images[3]}
+            height={93}
+            width={93}
+          />
+        </div>
+        <div className="product-details">
+          <h1 className="fs-1 product-title">{data.title}</h1>
+          <div className="product-description gray-color">
+            {data.description}
+          </div>
+          <div className="product-stock gray-color">In Stock: {data.stock}</div>
+          <div className="product-brand gray-color">Brand: {data.brand}</div>
+          <div className="product-category gray-color">
+            Category: {data.category}
+          </div>
+        </div>
       </div>
-      <div className="other-images d-flex justify-content-center gap-2 my-2 mx-1">
-        <Image
-          className="rounded-2"
-          src={data.images[1]}
-          height={93}
-          width={93}
-        />
-        <Image
-          className="rounded-2"
-          src={data.images[2]}
-          height={93}
-          width={93}
-        />
-        <Image
-          className="rounded-2"
-          src={data.images[3]}
-          height={93}
-          width={93}
-        />
-      </div>
-      <h1 className="fs-1 product-title">{data.title}</h1>
-      <div className="product-description">{data.description}</div>
-      <div className="product-rating"></div>
-      <div className="product-stock">In Stock: {data.stock}</div>
-      <div className="product-brand">Brand: {data.brand}</div>
-      <div className="product-category">Category: {data.category}</div>
       <Footer />
     </div>
   );
