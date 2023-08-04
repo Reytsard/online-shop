1c712ee01d31dfd0e25e8f39a22f9c2b4c37df38
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2023-08-04 12:42:39 +0800

    Added Redirect in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import "../styles/main.css";
 import Link from "next/link";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  useEffect(() => {
+    redirect("/folder1/page1");
+  }, [redirect]);
   return (
     <main className={styles.main}>
       <Link href={"/folder1/page1"}>Products</Link>
